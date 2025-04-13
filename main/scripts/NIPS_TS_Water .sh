5877bb2b104d012af20e702120eadf514daeda15
infogroup502 <energysmartcontrol@126.com>
2025-04-13 14:28:39 +0800

    Delete main directory

--- main/scripts/NIPS_TS_Water .sh
+++
@@ -1,4 +0,0 @@
-export CUDA_VISIBLE_DEVICES=0
-
-python main.py --anormly_ratio 0.5 --num_epochs 3   --batch_size 256  --dataset NIPS_TS_Water  --input_c 9  --win_size 92 --win_size_1 14 --count 8 --p 1
-
--- main/utils/utils.py
+++
@@ -1,8 +0,0 @@
-import os
-import torch
-from torch.autograd import Variable
-
-
-def mkdir(directory):
-    if not os.path.exists(directory):
-        os.makedirs(directory)
